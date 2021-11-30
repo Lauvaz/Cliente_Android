591cf0c1e69afcc94369b90c9f0b95dcaa4d4493
paudenn <pau2001r.b@gmail.com>
2021-11-30 13:03:27 +0100

    client

---
+++ app/src/main/java/edu/upc/dsa/client_g04/APIREST.java
@@ -0,0 +1,12 @@
+package edu.upc.dsa.client_g04;
+
+import retrofit2.*;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface APIREST {
+
+    @POST("user/register")
+    Call<User> addUser(@Body User user);
+
+}
---
+++ app/src/main/java/edu/upc/dsa/client_g04/Register.java
@@ -0,0 +1,6 @@
+package edu.upc.dsa.client_g04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+}
